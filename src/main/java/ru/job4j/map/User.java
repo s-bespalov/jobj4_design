2c37fa624b66c4b2ffbd2ec4bccd4ec92dc5bd3d
Sergey Bespalov <sergey.bespalov@protonmail.com>
2023-12-10 13:28:57 +0300

    3. Создать модель User [#999]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,7 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
+import java.text.DateFormat;
+import java.util.*;
 
 public class User {
     private String name;
@@ -11,5 +12,37 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        var alena = new User(
+                "Alena",
+                1,
+                new Calendar.Builder().setDate(2000, 6, 18).build());
+        var alena2 = new User(
+                "Alena",
+                1,
+                new Calendar.Builder().setDate(2000, 6, 18).build());
+        Map<User, Object> map = new HashMap<>();
+        map.put(alena, new Object());
+        map.put(alena2, new Object());
+        System.out.println(map);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 }
