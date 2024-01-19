7075c3689844e7deb8a4b718e5863862445db97e
Sergey Bespalov <sergey.bespalov@protonmail.com>
2024-01-20 00:41:44 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -13,5 +13,16 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+
+        boolean bool = true;
+        byte b = 8;
+        char c = 'h';
+        short s = 200;
+        int i = 30000;
+        long l = 9L;
+        float f = .1F;
+        double d = .1D;
+        LOG.debug("boolean: {}, byte: {}, char: {}, short: {}, int: {}, long: {}, float: {}, double: {}",
+                bool, b, c, s, i, l, f, d);
     }
 }
