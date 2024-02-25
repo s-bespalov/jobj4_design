a5aa46ead77ae6c4f16d100a01cfb71bdfc1579a
Sergey Bespalov <sergey.bespalov@protonmail.com>
2024-02-25 08:10:26 +0300

    0. Select [#6861]

---
+++ db/0_select/fauna_init.sql
@@ -0,0 +1,33 @@
+create table fauna
+(
+    id             serial primary key,
+    "name"           text,
+    avg_age        int,
+    discovery_date date
+);
+
+insert into fauna (name, avg_age, discovery_date) values ('Whale', 80, '2001-05-15');
+insert into fauna (name, avg_age, discovery_date) values ('Blue whale', 90, '1998-08-20');
+insert into fauna (name, avg_age, discovery_date) values ('Gray whale', 70, '2005-02-10');
+insert into fauna (name, avg_age, discovery_date) values ('Bottlenose dolphin', 25, '2010-11-30');
+insert into fauna (name, avg_age, discovery_date) values ('Orca', 50, '2008-06-25');
+insert into fauna (name, avg_age, discovery_date) values ('Common dolphin', 40, '2015-04-12');
+insert into fauna (name, avg_age, discovery_date) values ('Great white shark', 30, '2003-09-18');
+insert into fauna (name, avg_age, discovery_date) values ('Tiger shark', 25, '2007-07-05');
+insert into fauna (name, avg_age, discovery_date) values ('Hammerhead shark', 20, '2012-03-22');
+insert into fauna (name, avg_age, discovery_date) values ('Clownfish', 5, '2010-09-14');
+insert into fauna (name, avg_age, discovery_date) values ('Angelfish', 10, '2007-11-27');
+insert into fauna (name, avg_age, discovery_date) values ('Catfish', 15, '2005-04-03');
+insert into fauna (name, avg_age, discovery_date) values ('Starfish', 8, '2008-12-19');
+insert into fauna (name, avg_age, discovery_date) values ('Jellyfish', 2, '2013-06-08');
+insert into fauna (name, avg_age, discovery_date) values ('Cuttlefish', 3, '2016-10-22');
+insert into fauna (name, avg_age, discovery_date) values ('Giant Barrel Sponge', 20000, '1990-04-15');
+insert into fauna (name, avg_age, discovery_date) values ('Black Coral', 12000, '1985-10-22');
+insert into fauna (name, avg_age, discovery_date) values ('Posidonia Oceanica', 200000, '1978-03-07');
+insert into fauna (name, avg_age, discovery_date) values ('Blue Whale Sponge', 22000, '2000-12-10');
+insert into fauna (name, avg_age, discovery_date) values ('Giant Squid', 5, null);
+insert into fauna (name, avg_age, discovery_date) values ('Deep-sea Dragonfish', 10, null);
+insert into fauna (name, avg_age, discovery_date) values ('Yeti Crab', 8, null);
+insert into fauna (name, avg_age, discovery_date) values ('Coelacanth', 60, '1938-12-23');
+insert into fauna (name, avg_age, discovery_date) values ('Giant Pacific Octopus', 5, '1927-05-11');
+insert into fauna (name, avg_age, discovery_date) values ('Megamouth Shark', 20, '1933-09-05');
