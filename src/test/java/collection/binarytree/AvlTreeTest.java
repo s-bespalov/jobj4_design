bd89d9868f8a48d005dff0f1e2ad48f5bca43fe6
Sergey Bespalov <sergey.bespalov@protonmail.com>
2024-09-10 23:44:25 +0300

    Реализация балансировки узлов AVL-дерева [#504993 #512729]

--- src/test/java/collection/binarytree/AvlTreeTest.java
+++ src/test/java/collection/binarytree/AvlTreeTest.java
@@ -16,4 +16,130 @@ class AvlTreeTest {
         List<Integer> list = tree.inSymmetricalOrder();
         assertThat(list).containsExactly(1, 2, 3, 4, 5, 6, 7);
     }
+
+    @Test
+    void whenAddToEmptyTreeThenListContainsOneElement() {
+        AvlTree<String> tree = new AvlTree<>();
+        assertThat(tree.insert("first")).isTrue();
+        assertThat(tree.inSymmetricalOrder()).hasSize(1)
+                .containsExactly("first");
+    }
+
+    @Test
+    void whenAddTwoToEmptyTreeThenListContainsTwoElement() {
+        AvlTree<String> tree = new AvlTree<>();
+        assertThat(tree.insert("first")).isTrue();
+        assertThat(tree.insert("second")).isTrue();
+        assertThat(tree.inSymmetricalOrder()).hasSize(2)
+                .containsExactly("first", "second");
+    }
+
+    @Test
+    void whenAddMaximumNotEndThenOk() {
+        AvlTree<Integer> tree = new AvlTree<>();
+        for (int element : new int[]{4, 2, 6, 1, 3, 5, 8, 7}) {
+            tree.insert(element);
+        }
+        assertThat(tree.maximum()).isEqualTo(8);
+    }
+
+    @Test
+    void whenAddMaximumIsEndThenOk() {
+        AvlTree<Integer> tree = new AvlTree<>();
+        for (int element : new int[]{4, 2, 6, 1, 3, 5, 7}) {
+            tree.insert(element);
+        }
+        assertThat(tree.maximum()).isEqualTo(7);
+    }
+
+    @Test
+    void whenAddMinimumIsEndThenOk() {
+        AvlTree<Integer> tree = new AvlTree<>();
+        for (int element : new int[]{4, 2, 6, 3, 5, 7, 1}) {
+            tree.insert(element);
+        }
+        assertThat(tree.minimum()).isEqualTo(1);
+    }
+
+    @Test
+    void whenAddMinimumIsNotEndThenOk() {
+        AvlTree<Integer> tree = new AvlTree<>();
+        for (int element : new int[]{4, 2, 6, 3, 5, 7 }) {
+            tree.insert(element);
+        }
+        assertThat(tree.minimum()).isEqualTo(2);
+    }
+
+    @Test
+    void whenSymmetricalOrderThenOk() {
+        AvlTree<Integer> tree = new AvlTree<>();
+        for (int element : new int[]{4, 2, 6, 3, 5, 7, 1}) {
+            tree.insert(element);
+        }
+        assertThat(tree.inSymmetricalOrder()).hasSize(7)
+                .containsExactly(1, 2, 3, 4, 5, 6, 7);
+    }
+
+    @Test
+    void whenPreOrderThenOk() {
+        AvlTree<Integer> tree = new AvlTree<>();
+        for (int element : new int[]{4, 2, 6, 3, 5, 7, 1}) {
+            tree.insert(element);
+        }
+        assertThat(tree.inPreOrder()).hasSize(7)
+                .containsExactly(4, 2, 1, 3, 6, 5, 7);
+    }
+
+    @Test
+    void whenPostOrderThenOk() {
+        AvlTree<Integer> tree = new AvlTree<>();
+        for (int element : new int[]{4, 2, 6, 3, 5, 7, 1}) {
+            tree.insert(element);
+        }
+        assertThat(tree.inPostOrder()).hasSize(7)
+                .containsExactly(1, 3, 2, 5, 7, 6, 4);
+    }
+
+    @Test
+    void whenRemove1ThenOk() {
+        AvlTree<Integer> bst = new AvlTree<>();
+        int[] array = new int[]{2, 1, 10, 6, 14, 4, 8, 12, 16, 11, 9, 13, 15, 17, 3, 5, 7};
+        for (int i : array) {
+            bst.insert(i);
+        }
+        bst.remove(1);
+        var expected = List.of(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17);
+        var actual = bst.inSymmetricalOrder();
+        assertThat(actual).containsExactlyElementsOf(expected);
+    }
+
+    @Test
+    void whenRemove2ThenOk() {
+        AvlTree<Integer> bst = new AvlTree<>();
+        int[] array = new int[]{2, 1, 10, 6, 14, 4, 8, 12, 16, 11, 9, 13, 15, 17, 3, 5, 7};
+        for (int i : array) {
+            bst.insert(i);
+        }
+        bst.remove(2);
+        var expected = List.of(1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17);
+        var actual = bst.inSymmetricalOrder();
+        assertThat(actual).containsExactlyElementsOf(expected);
+    }
+
+    @Test
+    void whenRemove10And14And16And8And2ThenOk() {
+        AvlTree<Integer> bst = new AvlTree<>();
+        int[] array = new int[]{2, 1, 10, 6, 14, 4, 8, 12, 16, 11, 9, 13, 15, 17, 3, 5, 7};
+        for (int i : array) {
+            bst.insert(i);
+        }
+        bst.remove(10);
+        bst.remove(14);
+        bst.remove(2);
+        bst.remove(16);
+        bst.remove(8);
+        var expected = List.of(1, 3, 4, 5, 6, 7, 9, 11, 12, 13, 15, 17);
+        var actual = bst.inSymmetricalOrder();
+        assertThat(actual).containsExactlyElementsOf(expected);
+    }
 }
